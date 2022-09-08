3b0a651efd075180fa95ab2cab710fec0d05384f
LoveWebWithJS <105291402+LoveWebWithJS@users.noreply.github.com>
2022-09-08 18:35:41 +0300

    Add files via upload
    upload project

---
+++ JS/script.js
@@ -0,0 +1,11 @@
+//! Определение литералов
+// Ширина доступного пространства
+const availableScreenWidth = window.screen.availWidth;
+// Высота доступного пространства
+const availableScreenHeight = window.screen.availHeight;
+
+
+
+//! Основной функционал сайта
+
+
