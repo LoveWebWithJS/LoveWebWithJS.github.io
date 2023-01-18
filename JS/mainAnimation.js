37bb480e4d78c8c38267b7ee66021d897dc8819d
LoveWebWithJS <melnikaa2010@gmail.com>
2023-01-18 18:29:52 +0300

    fix

--- JS/mainAnimation.js
+++ JS/mainAnimation.js
@@ -1,1 +1,9 @@
-//! Будем делать анимацтю для иконок (в качестве обучения), чтоб поворачивались на 360 при hover-е)))
+//! Будем делать анимацтю для иконок (в качестве обучения), чтоб поворачивались на 360 при hover-е)
+function rotate360() {
+	this.classList.add('.rotate360');
+}
+
+let iconsHeader = document.querySelectorAll(".logo");
+// console.log(iconsHeader);
+addEventListener('mouseover', rotate360);
+
